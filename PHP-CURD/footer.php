22c9fef342927d278db31ba059cd170e7040167b
susirahma <susserhmtw@gmail.com>
2025-01-10 13:28:22 +0700

    php

---
+++ PHP-CURD/footer.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+    <footer class="blockquote-footer fixed-bottom"> Rekayasa Perangkat Lunak 2023 <cite title="Source Title"><a
+href="https://polibang.ac.id"
+target="_blank">polibang.ac.id</a></cite></footer>
+    </body>
+</html>
